753edf9447b0b4178bd7242a92e0ff55a9a96d42
peeperklip <osamaboujdi@gmail.com>
2020-01-16 11:41:05 +0100

    Demo configuration

--- index.php
+++ index.php
@@ -2,19 +2,7 @@
 /** @noinspection ALL */
 require_once __DIR__ . '/bootstrap.php';
 
-//Serialize
-$opelAstra = new Car('Opel', 'Astra');
-$jsonData = $serializer->serialize($opelAstra, 'json');
-$xmlData = $serializer->serialize($opelAstra, 'xml');
-
-//Schrijf het resultaat naar een file
-file_put_contents("xmldata.xml", $xmlData);
-file_put_contents("jsondata.json", $jsonData);
-
-// Deserialize
-$objectOne = $serializer->deserialize(file_get_contents('jsondata.json'), Car::class,'json');
-$objectTwo = $serializer->deserialize(file_get_contents('xmldata.xml'), Car::class,'xml');
-
-echo $objectOne;
-echo PHP_EOL;
-echo $objectTwo;
+//Deserialize
+$jsonData = file_get_contents(__DIR__ . '/car.json');
+$car = $serializer->deserialize($jsonData, Car::class, 'json');
+echo $car;
