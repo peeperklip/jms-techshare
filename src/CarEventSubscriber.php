c043c250d4b4cbec865243ea33b6e9a49c0efe37
peeperklip <osamaboujdi@gmail.com>
2020-01-16 13:17:41 +0100

    Add a few events

--- src/CarEventSubscriber.php
+++ src/CarEventSubscriber.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Car;
 use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+use JMS\Serializer\EventDispatcher\ObjectEvent;
 use JMS\Serializer\EventDispatcher\PreSerializeEvent;
 
 class CarEventSubscriber implements EventSubscriberInterface
@@ -19,9 +20,23 @@ class CarEventSubscriber implements EventSubscriberInterface
         'event' => DefinedEvents::PRE_SERIALIZE,
         'method' => 'onPreSerialize',
         'class' => Car::class,
-        'format' => 'json',
         'priority' => 0,
-      ]
+      ],[
+        'event' => DefinedEvents::POST_SERIALIZE,
+        'method' => 'onPostSerialize',
+        'class' => Car::class,
+        'priority' => 0,
+      ],[
+        'event' => DefinedEvents::PRE_DESERIALIZE,
+        'method' => 'onPreDeserialize',
+        'class' => Car::class,
+        'priority' => 0,
+      ],[
+        'event' => DefinedEvents::POST_DESERIALIZE,
+        'method' => 'onPostDeserialize',
+        'class' => Car::class,
+        'priority' => 0,
+      ],
     ];
   }
 
@@ -30,5 +45,28 @@ class CarEventSubscriber implements EventSubscriberInterface
   {
     echo __FUNCTION__ . ' ON ' . get_class($event->getObject());
     echo __FILE__ . ':' . __LINE__ . PHP_EOL;
+    echo '<br>';
   }
+
+  public function onPostSerialize(ObjectEvent $event)
+  {
+    echo __FUNCTION__ . ' ON ' . get_class($event->getObject());
+    echo __FILE__ . ':' . __LINE__ . PHP_EOL;
+    echo '<br>';
+  }
+
+  public function onPreDeserialize(ObjectEvent $event)
+  {
+    echo __FUNCTION__ . ' ON ' . get_class($event->getObject());
+    echo __FILE__ . ':' . __LINE__ . PHP_EOL;
+    echo '<br>';
+  }
+
+  public function onPostDeserialize(ObjectEvent $event)
+  {
+    echo __FUNCTION__ . ' ON ' . get_class($event->getObject());
+    echo __FILE__ . ':' . __LINE__ . PHP_EOL;
+    echo '<br>';
+  }
+
 }
